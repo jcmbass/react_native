d9ba43d818ee124f3d11d467ccb5ca4408dc562c
Josue Montecinos <jcmbass@gmail.com>
2021-01-11 18:31:46 -0600

    Alphabetize Sections

---
+++ course/4-list-input/AddContactForm.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import {Button, StyleSheet, TextInput, View} from 'react-native'
+import PropTypes from 'prop-types'
+
+const styles = StyleSheet.create({
+  input: {
+    padding: 5,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+})
+
+export default class AddContactForm extendes React.Component {
+  static propTypes = {
+    addContact: PropTypes.func,
+  }
+
+  state = {
+    name: '',
+    phone: '',
+  }
+
+  handleNameChange = name => {
+    this.setState({name})
+  }
+
+  handlePhoneChange = phone => {
+    this.setState({phone})
+  }
+
+  render() {
+    return (
+      <View style={{paddingTop: 20}}>
+        <TextInput
+          style={styles.input}
+          onChangeText={this.handleNameChange}
+          value={this.state.name}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={this.handlePhoneChange}
+          value={this.state.phone}
+          keyboarType="numeric"
+        />
+        <Button title="Add Contact" />
+      </View>
+    )
+  }
+}
--- course/4-list-input/App.js
+++ course/4-list-input/App.js
@@ -1,29 +1,43 @@
 import React from 'react';
-import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Button, SectionList, StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants'
 
-import contacts from './contacts'
+import contacts, {compareNames} from './contacts'
 import Row from './Row'
+import ContactList from './contactList'
+import AddContactForm from './AddContactForm'
 
 export default class App extends React.Component {
   state = {
     showContacts: false,
+    showForm: false,
+    contacts: contacts,
   }
+
 
   toggleContacts = () => {
     this.setState(prevState => ({showContacts: !prevState.showContacts}))
   }
 
+  toggleForm = () => {
+    this.setState(prevState => ({shoForm: !prevState.shoForm}))
+  }
+
+  sort = () => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts].sort(compareNames)
+    }))
+  }
+
   render() {
+    if (this.state.showForm) return <AddContactForm />
+
     return (
       <View style={styles.container}>
-        <Button title="toggle contacts"
-        onPress={this.toggleContacts} />
-        {this.state.showContacts && (
-          <ScrollView>
-            {contacts.map(contact => <Row {...contact} />)}
-          </ScrollView>
-        )}
+        <Button title="toggle contacts" onPress={this.toggleContacts} />
+        <Button title="Add contact" onPress={this.toggleForm} />
+          {this.state.showContacts && <ContactList contacts={this.state.contacts}
+       />}
       </View>
     );
   }
